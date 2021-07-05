f4093f490c48f546508ec6614f512e25ab533398
SoniaStalance <soniastalance@gmail.com>
2021-07-05 22:19:25 +0530

    build changes

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ connectDB();
 app.get('/',(req,res)=>res.send('API Running'));
 
 app.use(express.json({extended:false}));
-
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
